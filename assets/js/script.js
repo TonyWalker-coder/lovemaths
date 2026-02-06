9cd45efe3d1b6a8bad83100a6c0aea563544729e
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-02-06 21:54:34 +0000

    added the addtion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,71 @@
-//
-//wait for the DOM to load before running
-//
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.getElementsByTagName("button");
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-  for (let button of buttons) {
-    button.addEventListener("click", function () {
-      if (this.getAttribute("data-type") === "submit") {
-        alert("You clicked Submit!");
-      } else {
-        let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
-      }
-    });
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+
 });
 
 /**
- * the main game loop call when the script first loads
- * and after the user's answer has been processed 
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function RunGame() {
-  //create 2 random numbers between 1 and 25
-  let num1 =Math.floor(Math.random()*25)+1;
-  let num2 =Math.floor(Math.random()*25)+1;
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
-function CheckAnswer() {}
+function checkAnswer() {
 
-function CalculateCorrectAnswer() {}
+}
 
-function incrementScore() {}
+function calculateCorrectAnswer() {
 
-function incrementWrongAnswer() {}
+}
 
-function displayAdditionQuestion() {}
+function incrementScore() {
 
-function displaySubtractQuestion() {}
+}
 
-function displayMultiplyQuestion() {}
+function incrementWrongAnswer() {
 
-function displayDivisionQuestion() {}
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
