edfa02a7fea3f29bd91039e4d46461033e814050
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-02-06 16:56:04 +0000

    added script structure

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function RunGame() {}
+
+function CheckAnswer() {}
+
+function CalculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}   
+
+function displayMultiplyQuestion() {}
+
+function displayDivisionQuestion() {}
